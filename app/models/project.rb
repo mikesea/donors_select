41316a00bac3638d50d0d5d9a780cd3038e4ccd1
Elise Worthy <elise.worthy@livingsocial.com>
2012-07-16 18:35:10 -0400

    formatting for project grid based on number of projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,32 @@ require 'net/http'
 
 class Project
   def self.test_data
-    base_uri = 'http://api.donorschoose.org/common/json_feed.html?state=WA'
-    JSON.parse(Net::HTTP.get(URI(base_uri)))
+
+    counter_params = [
+                       "&index=51&max=50",
+                       "&index=101&max=50",
+                     ]
+
+    first_fifty = retrieve_data('http://api.donorschoose.org/common/json_feed.html?state=WA&max=50')
+    first_fifty = JSON.parse(first_fifty)
+
+    counter_params.each do |param|
+      response = retrieve_data('http://api.donorschoose.org/common/json_feed.html?state=WA' + param)
+      json_projects = JSON.parse(response)
+      add_project(json_projects['proposals'], first_fifty['proposals'])
+    end
+    first_fifty
   end
 
+  private
+
+  def self.retrieve_data(uri)
+    Net::HTTP.get(URI(uri))
+  end
+
+  def self.add_project(projects_to_add, master)
+    projects_to_add.each do |project|
+      master << project
+    end
+  end
 end
