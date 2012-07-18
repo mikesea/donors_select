cca7ebe5df2d552e3b8f4699776d7a51a158c3a4
Mike Chlipala <mchlipala@gmail.com>
2012-07-18 16:19:40 -0400

    resque jobs for fetching and publishing projects

--- app/jobs/fetcher.rb
+++ app/jobs/fetcher.rb
@@ -1,7 +1,20 @@
 class Fetcher
   @queue = :projects
 
-  def self.perform(uri)
-    Project.redis.set uri, JSON.parse(Net::HTTP.get(URI(uri))).to_json
+  def self.perform(uri, user_token)
+    unless Project.redis.get uri
+      Project.redis.set uri, JSON.parse(Net::HTTP.get(URI(uri))).to_json
+    end
+    publish(JSON.parse(Project.redis.get(uri))["proposals"], user_token)
+  end
+
+private
+
+  def self.publish(projects_json, user_token)
+    Pusher[user_token].trigger(
+      'projects_fetch', {
+        projects: projects_json
+      }
+    )
   end
 end
