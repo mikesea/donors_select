7b5eb79f12579e401caa13237b6d4e28145c55a5
Mike Chlipala <mchlipala@gmail.com>
2012-07-20 18:44:01 -0400

    another refactor to projectscontroller#index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,8 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.find_by params[:filters]
-    unless @projects
-      Project.fetch_and_publish params[:filters], cookies.signed[:token]
+    unless @projects = Project.find_by(params[:filters])
+      Project.fetch_and_publish(params[:filters], cookies.signed[:token])
     end
   end
 
