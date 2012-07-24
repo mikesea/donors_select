792626abaf667c37c8a29c6aaced27936bedf625
Mike Chlipala <mchlipala@gmail.com>
2012-07-24 16:44:47 -0400

    requests specs

--- spec/requests/projects_counts_spec.rb
+++ spec/requests/projects_counts_spec.rb
@@ -1,29 +1,29 @@
 require 'spec_helper'
 
-describe "ProjectsController" do
+describe "ProjectsCountsController" do
   describe "#index" do
 
     let(:uri) { Project.build_uri }
     let(:projects) { serve_response(:projects) }
 
-    context "fetching projects that have been cached" do
+    context "fetching counts that have been cached" do
 
       before(:each) do
         Project.stub(:find_by).and_return JSON.parse(projects)
-        get projects_url(format: :json)
+        get projects_counts_url(format: :json)
       end
 
-      it "returns a set of projects" do
+      it "returns a count of projects" do
         response_json = JSON.parse(response.body)
-        response_json.should_not be nil
+        response_json["count"].should_not be nil
+        response_json["count"].class.should be Fixnum
       end
     end
 
     context "fetching projects that haven't been cached" do
       before(:each) do
         Project.stub(:find_by).and_return nil
-        Resque.should_receive(:enqueue)
-        get projects_url(format: :json)
+        get projects_counts_url(format: :json)
       end
 
       it "returns no projects and queues a job to fetch them" do
