03b14320bccacef4b041309f53b0e5d21aed991d
Jacqueline Chenault <mchlipala+adigitalnative@gmail.com>
2012-07-25 14:20:21 -0400

    refactor project model; closes #7; closes #10

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe "Project" do
   context "fetching projects from DonorsChoose" do
     describe ".find_by" do
-      context "and the query has not been cached in redis" do
+      context "and the query has not been cached" do
       
         before(:each) do
-          Redis.any_instance.stub(:get).and_return nil
+          Project.stub(:fetch_projects).and_return nil
         end
 
         it "returns nil" do
@@ -15,9 +15,9 @@ describe "Project" do
         end
       end
 
-      context "and the query has been cached in redis" do
+      context "and the query has been cached" do
         before(:each) do
-          Redis.any_instance.stub(:get).and_return serve_response(:projects)
+          Project.stub(:fetch_projects).and_return serve_response(:projects)
         end
 
         it "returns the cached projects" do
@@ -30,12 +30,27 @@ describe "Project" do
     describe ".fetch_and_publish" do
       let(:uri) { Project.build_uri(["state=MN"]) }
 
-      before(:each) do
-        Resque.should_receive(:enqueue).with(Fetcher, uri, "123")
-      end
-
-      it "queues a resque job" do
+      it "calls queue job with the uri and token" do
+        Project.should_receive(:queue_fetch_job).with(uri, "123")
         Project.fetch_and_publish(["state=MN"], "123")
+      end
+    end
+
+    describe ".fetch_projects" do
+      let(:uri) { Project.build_uri(["state=MN"]) }
+
+      it "calls redis.get with the uri" do
+        Redis.any_instance.should_receive(:get).with(uri)
+        Project.fetch_projects(uri)
+      end
+    end
+
+    describe ".queue_fetch_job" do
+      let(:uri) { Project.build_uri(["state=MN"]) }
+
+      it "calls Resque.enqueue with the uri and token" do
+        Resque.should_receive(:enqueue).with(Fetcher, uri, "123")
+        Project.queue_fetch_job(uri, "123")
       end
     end
   end
