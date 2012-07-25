0b2a0a22f8968d2e6667fb91842b27004cde5edf
Mike Chlipala <mchlipala@gmail.com>
2012-07-25 14:40:42 -0400

    use session_token helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :set_session
+  helper_method :session_token
 
   def set_session
     self.session_token || self.session_token = {
       value: rand(36**8).to_s(36),
-      expires: 1.hour.from_now
+      expires: 1.week.from_now
     }
   end
 
