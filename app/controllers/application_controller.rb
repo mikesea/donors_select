f7c4d1370f92ccfe01c6b377fa9cf1cea9569e67
Charles Strahan <charles.c.strahan@gmail.com>
2012-07-25 11:04:33 -0400

    Factour out session_token - closes #13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   before_filter :set_session
 
   def set_session
-    unless cookies.signed[:token]
-      cookies.signed[:token] = { value: rand(36**8).to_s(36),
-        expires: 1.hour.from_now
-      }
-    end
+    self.session_token || self.session_token = {
+      value: rand(36**8).to_s(36),
+      expires: 1.hour.from_now
+    }
+  end
+
+  def session_token=(token)
+    cookies.signed[:token] = token
+  end
+
+  def session_token
+    cookies.signed[:token]
   end
 end
